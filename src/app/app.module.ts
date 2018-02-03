dca70e051fc87b41d4b99c51bae7925869dc8cf4
bradtaniguchi <bradtaniguchi@gmail.com>
2018-02-03 12:13:40 -0800

    Added landing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { LandingModule } from './landing/landing.module';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LandingModule
+  ],
+  declarations: [
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
