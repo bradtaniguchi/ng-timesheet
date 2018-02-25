6c9e0a681bcbf62e8999c5486eb4172abef54683
bradtaniguchi <bradtaniguchi@gmail.com>
2018-02-24 16:05:51 -0800

    started adding module for the timesheet list.

--- src/app/timesheet-list/timesheet-list.module.ts
+++ src/app/timesheet-list/timesheet-list.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimesheetListComponent } from './timesheet-list.component';
+import { MatTableModule, MatCardModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
+    // angular material
+    MatTableModule,
+    CdkTableModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   exports: [
     TimesheetListComponent
