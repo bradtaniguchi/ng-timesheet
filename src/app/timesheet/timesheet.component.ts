1f986218273aaba899f70315ab1fc7540b9b1f10
bradtaniguchi <bradtaniguchi@gmail.com>
2018-02-27 23:02:00 -0800

    Started adding timesheet
    - this page will use the basic responsive layouts which will be used
      elsewhere on the app.

--- src/app/timesheet/timesheet.component.ts
+++ src/app/timesheet/timesheet.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TimesheetService } from '../services/timesheet/timesheet.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Timesheet } from '../interfaces/timesheet';
 
 @Component({
   selector: 'app-timesheet',
   templateUrl: './timesheet.component.html',
-  styles: []
+  styles: [
+    ':host { height: 100%; overflow: auto;}'
+  ]
 })
 export class TimesheetComponent implements OnInit {
-
-  constructor() { }
+  edit: boolean;
+  @ViewChild('form') form: NgForm;
+  timesheet: Timesheet;
+  constructor(
+    private timesheetService: TimesheetService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-  }
+    console.log('in timesheet', this.form);
 
+    // for not timesheet will always be "new"
+    this.timesheet = <any>{};
+  }
+  cancel() {
+    this.router.navigate(['../']);
+  }
 }
--- src/app/timesheet/timesheet.module.ts
+++ src/app/timesheet/timesheet.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimesheetComponent } from './timesheet.component';
+import { MatCardModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+
+    FlexLayoutModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   exports: [
     TimesheetComponent
