c8d9f5a51efd2b7eee92f0d66ddc2657fdbbaab0
bradtaniguchi <bradtaniguchi@gmail.com>
2018-05-07 21:52:34 -0700

    Installed prettier to run in pre-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,55 +1,55 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-import { TimesheetListComponent } from './timesheet-list/timesheet-list.component';
-import { TimesheetComponent } from './timesheet/timesheet.component';
-import { AuthGuard } from './guards/auth.guard';
-import { Page404Component } from './page-404/page-404.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LandingComponent } from "./landing/landing.component";
+import { TimesheetListComponent } from "./timesheet-list/timesheet-list.component";
+import { TimesheetComponent } from "./timesheet/timesheet.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { Page404Component } from "./page-404/page-404.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     canActivate: [AuthGuard],
     children: [
       {
-        path: '',
-        redirectTo: '/sheets',
-        pathMatch: 'full'
+        path: "",
+        redirectTo: "/sheets",
+        pathMatch: "full"
       },
       {
-        path: 'sheets',
+        path: "sheets",
         component: TimesheetListComponent
       },
       {
-        path: 'sheet',
+        path: "sheet",
         component: TimesheetComponent
       },
       {
-        path: 'sheet/:id',
+        path: "sheet/:id",
         component: TimesheetComponent
       },
       {
-        path: 'view/:id',
+        path: "view/:id",
         loadChildren:
-          'app/timesheet-view/timesheet-view.module#TimesheetViewModule'
+          "app/timesheet-view/timesheet-view.module#TimesheetViewModule"
       }
     ]
   },
   {
-    path: 'landing',
+    path: "landing",
     component: LandingComponent
   },
   {
-    path: 'login',
-    loadChildren: 'app/login/login.module#LoginModule'
+    path: "login",
+    loadChildren: "app/login/login.module#LoginModule"
   },
   {
-    path: '404',
+    path: "404",
     component: Page404Component
   },
   {
-    path: '**',
-    redirectTo: '404'
+    path: "**",
+    redirectTo: "404"
   }
 ];
 
